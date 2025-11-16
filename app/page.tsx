1bb5bdc9ad5a7d766daca08c321630a3a321a6fc
arundeshan@gmail.com <arundeshan@gmail.com>
2025-11-16 10:13:39 +0530

    feat: initialize project with Next.js, TypeScript, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, and linting.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added SVG assets for icons and branding (file.svg, globe.svg, next.svg, vercel.svg, window.svg).
    - Configured TypeScript with tsconfig.json for strict type checking and module resolution.

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center font-sans">
+      <Button variant={"default"}>Click me</Button>
+    </div>
+  );
+}
