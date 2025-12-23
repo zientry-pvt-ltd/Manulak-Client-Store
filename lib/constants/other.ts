0e52c8bfa6e8354bb1cf1607cc4d737d33ee32dc
arun68 <arundeshan@gmail.com>
2025-12-23 22:41:55 +0530

    feat: add COMPANY_INFO constant for centralized company details in AppFooter

---
+++ lib/constants/other.ts
@@ -0,0 +1,9 @@
+export const COMPANY_INFO = {
+  NAME: "Manulak Agro",
+  ADDRESS: "1234 Farm Lane, Agriculture City, Country",
+  EMAIL: "info@manulakagro.com",
+  PHONE: "+1 (234) 567-8900",
+  YOUTUBE_CHANNEL: "https://youtube.com/@manulakagro?si=EwiRUNaUBhedlUSe",
+  FACEBOOK_PAGE:
+    "https://www.youtube.com/redirect?event=channel_description&redir_token=QUFFLUhqbXg5eDEzNU5KRlM5NE9oVTJkdzNLdjNaUHBUQXxBQ3Jtc0ttTjVOcHVaWDdzOWJ3UTlLUG1hVEZQT2plTThJRlQ1UkpBRk8tXzNpUW5hX1JRZm84bUxjZEJsQVp0RG1fVnc1Uzk3OEF4WFRUb0RPT2h4N3FEZ09iWUJfQ3VxWGdaXzJTZW9uVGRKSjZiQ01UUGpuYw&q=https%3A%2F%2Fwww.facebook.com%2FAgri-School-110373631518870%2F%3Fref%3Dpages_you_manage",
+} as const;
